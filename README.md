67d4555dac0f609e4897eeefc4989ed164265a05
gorilla-kim <00ghks22@naver.com>
2019-07-27 16:39:01 +0000

    Web chat: using socket.io[nodeJS]

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # nodeJS-WebChat
+
+## 간단요약 설명
+* node.js 공부용 실습파일입니다.
+* socket.io 를 이용해 채팅을 구현하였습니다.
+* 유저 이름과 방이름을 받아 동일한 이름의 방에서 유저들이 소통합니다.
+
+## 사용기술
+* Language : javascript, html, css
+* Back : Node.js
+	* Lib :  Socket.io
+* Front : HTML, CSS, JS
+	* Lib : Jquery, Jquery Mobile
+	
+`참고서적 : 모던웹을 위한 Node.js`
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var socketio = require('socket.io');
 
+// 서버 생성
 var server = http.createServer(function(request, response) {
 	fs.readFile('index.html', function(error, data){
 		response.writeHead(200, { 'Content-Type': 'text/html' });
@@ -11,11 +12,15 @@ var server = http.createServer(function(request, response) {
 	console.log("Server Running at http://127.0.0.1:52273");
 });
 
+// 소켓에 서버 연결
 var io = socketio.listen(server);
 var roomList = [];
+// 새로운 client 가 연결됨을 감지합니다. connection
 io.sockets.on('connection', function(socket){
+	// 방이름을 받을 변수
 	var roomName = null;
 	
+	// 방 생성 혹은 특정 이름의 방에참가
 	socket.on('join', function(data){
 		console.log("============success!!\n\n");
 		roomName = data;
@@ -23,11 +28,11 @@ io.sockets.on('connection', function(socket){
 		console.log(roomList);
 		socket.join(data);
 	});
-	
+	// 같은 방의 유저들에게 메시지 전달
 	socket.on('message', function(data){
 		io.sockets.in(roomName).emit('message', data);
     });
-	
+	// 방 목록 받아보기
 	socket.on('getRoomList', function(data){
 		socket.emit('roomList', roomList);
     });
